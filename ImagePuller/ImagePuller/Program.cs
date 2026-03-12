67cb7d8d5a04e5915c1220ca1a3de1ddd5405831
Mattias Makra <mattias@makra.org>
2026-03-12 20:45:19 +0100

    feat: Configure ASP.NET Core for webhook model binding and buffering

--- ImagePuller/ImagePuller/Program.cs
+++ ImagePuller/ImagePuller/Program.cs
@@ -2,11 +2,22 @@ using ImagePuller.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Using standard .NET Logging
+// Register the new .NET 10 validation services
+builder.Services.AddValidation();
+
+// Standard Logging
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 
 var app = builder.Build();
+
+// Enable buffering so we can read the body twice 
+// (once for record validation, once for HMAC signature)
+app.Use((context, next) =>
+{
+    context.Request.EnableBuffering();
+    return next();
+});
 
 app.UseHttpsRedirection();
 
