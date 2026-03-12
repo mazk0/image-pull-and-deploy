35fe0601dbfe4b4c309384343510644fcfa48de2
Mattias Makra <mattias@makra.org>
2026-03-12 17:17:51 +0100

    feat: Integrate Serilog and application configuration

--- ImagePuller/ImagePuller/Program.cs
+++ ImagePuller/ImagePuller/Program.cs
@@ -1,7 +1,20 @@
+using ImagePuller.Endpoints;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure Serilog
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
+app.UseSerilogRequestLogging();
+
+// Register Endpoints
+app.MapWebhookEndpoints();
 
 app.Run();
